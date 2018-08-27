8f18b64063e5983ad8034ab61b34f4fbd7ee416e
Ehsan Gazar <me@ehsangazar.com>
2018-08-27 16:52:30 +1000

    Logout Scenario

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
 import styled from 'styled-components';
+import Link from 'next/link'
+
+// Amplify AWS Congnito
+import Amplify, { Auth } from 'aws-amplify';
+import CognitoConfig from '../Configs/Cognito.config'
+Amplify.configure(CognitoConfig);
 
 const Title = styled.h1`
   color: red;
@@ -7,16 +13,68 @@ const Title = styled.h1`
 
 interface Props {}
 
-interface State {}
+interface State {
+  user: Object,
+}
 
 class Index extends React.Component<Props,State> {
+  constructor(props:any) {
+    super(props)
+    this.state = {
+      user: {},
+    }
+  }
+  componentDidMount() {
+    // if user is logged in! go to the first page
+    Auth.currentAuthenticatedUser()
+    .then((response?:any)=>{
+      this.setState({
+        user: response.attributes
+      })
+    }).catch(()=>{
+      // do nothing
+    })
+  }
+
+  private _handleLogOut = () => {
+    Auth.signOut()
+    .then(() => {
+      window.location.reload()
+    })
+    .catch(() => {
+      window.location.reload()
+    })
+  }
+
   render () {
+    const { user = {} } = this.state
     return (
       <div>
         <Title>
           Welcome
-        </Title>
-        to NextJs TypeScript StyledComponent
+        </Title>   
+
+        {user && user.email &&
+          <div>
+            <h2>
+              You are logged in as {user.email}
+            </h2>
+            <button
+              onClick={this._handleLogOut}
+            >
+              Log out
+            </button>
+          </div>
+        }    
+
+        <br />
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+        <br />
+        <Link href="/register">
+          <a>register</a>
+        </Link>
       </div>
     )
   }
